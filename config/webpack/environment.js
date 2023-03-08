5082c21b59c087030a56cad2c85b08289cf06a56
sazumy <azumisaito@gmail.com>
2023-03-08 15:43:33 +0900

    node-sassをdart-sassに変える

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,4 +8,11 @@ environment.config.merge({
     },
   },
 })
+
+const sassLoader = environment.loaders.get('sass')
+const sassLoaderConfig = sassLoader.use.find(function (element) {
+  return element.loader === 'sass-loader'
+})
+sassLoaderConfig.options.implementation = require('sass')
+
 module.exports = environment
