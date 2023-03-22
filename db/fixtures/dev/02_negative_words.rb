cfedfedc5aa77a6600e3ab835a43f070df0c9f65
sazumy <azumisaito@gmail.com>
2023-03-22 12:04:21 +0900

    WIP seedファイルを追加

---
+++ db/fixtures/dev/02_negative_words.rb
@@ -0,0 +1,11 @@
+Dir.glob("#{Rails.root}/db/fixtures/dev/csv/negative_words.csv") do |f|
+  CSV
+    .read(f, headers: true)
+    .each do |row|
+      NegativeWord.seed do |s|
+        s.id = row[2]
+        s.content = row[0]
+        s.kana = row[1]
+      end
+    end
+end
