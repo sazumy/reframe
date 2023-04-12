9ed04adf1fb0df5b14e474f2c3b909adc47819bc
sazumy <azumisaito@gmail.com>
2023-04-12 13:34:51 +0900

    negative_wordのseedファイル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+CSV.foreach('db/seeds/csv/negative_words.csv') do |row|
+  NegativeWord.create(content: row[0], kana: row[1], id: row[2])
+end
