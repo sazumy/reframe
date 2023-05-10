dec621e710eb1a3497f994941cb9843dec615aed
sazumy <azumisaito@gmail.com>
2023-05-10 14:45:57 +0900

    LayoutFogとLayoutTriangleのスタイルを別ファイルに分ける

--- app/javascript/src/components/layouts/LayoutFog.tsx
+++ app/javascript/src/components/layouts/LayoutFog.tsx
@@ -1,14 +1,5 @@
 import React from 'react'
 
 export const LayoutFog: React.FC = ({ children }) => {
-  return (
-    <>
-      <div className="triangle_wrap">
-        <div className="triangle_right" />
-        <div className="triangle_left" />
-      </div>
-
-      {children}
-    </>
-  )
+  return <div className="fog">{children}</div>
 }
