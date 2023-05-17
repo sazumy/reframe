e1942632252aa6f72571b1b3115bbe37d4abbd18
sazumy <azumisaito@gmail.com>
2023-05-17 17:33:49 +0900

    userのseedの記載漏れ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-table_names = %i[negative_words positive_words word_connections]
+table_names = %i[negative_words positive_words word_connections users]
 
 table_names.each do |table_name|
   load(File.join(Rails.root, 'db', 'seeds', "#{table_name}.rb"))
