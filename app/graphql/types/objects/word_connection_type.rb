c6f1b45b508fd8f8728925194926d589db39eeb9
sazumy <azumisaito@gmail.com>
2023-05-17 15:11:48 +0900

    UuidTypeをIDに変更

--- app/graphql/types/objects/word_connection_type.rb
+++ app/graphql/types/objects/word_connection_type.rb
@@ -3,8 +3,8 @@
 module Types
   class Objects::WordConnectionType < Types::BaseObject
     field :id, ID, null: false
-    field :negative_word_id, Types::UuidType, null: false
-    field :positive_word_id, Types::UuidType, null: false
+    field :negative_word_id, ID, null: false
+    field :positive_word_id, ID, null: false
     field :order, Integer
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
