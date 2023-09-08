78e85d947b7a949931978b2d393644dec0ef1399
sazumy <azumisaito@gmail.com>
2023-09-08 14:38:35 +0900

    ドロップダウンメニューのスタイル調整

--- app/javascript/src/components/organisms/Menu/DropDownMenu.tsx
+++ app/javascript/src/components/organisms/Menu/DropDownMenu.tsx
@@ -5,12 +5,12 @@ export const DropDownMenu: React.VFC = () => {
   return (
     <ul className="dropdown__menu">
       <li className="dropdown__menu--list">
-        <Link to="/users/mypage" className="">
+        <Link to="/users/mypage" className="link">
           マイページ
         </Link>
       </li>
       <li className="dropdown__menu--list">
-        <a href="/users/sign_out" data-method="delete" className="">
+        <a href="/users/sign_out" data-method="delete" className="link">
           サインアウト
         </a>
       </li>
